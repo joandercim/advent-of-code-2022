3cda116bc9e687743bc9ce5648fc26b3247add92
joandercim <joacim@josiasmusik.se>
2023-11-01 07:27:32 +0100

    started with day 3 part 2

--- js/bundle.js
+++ js/bundle.js
@@ -1,7 +1,9 @@
 import Game from './Game.js'
 import GamePart2 from './Game2.js'
 import Day3 from './Day3.js';
+import Day3Part2 from './Day3-2.js';
 
 const game = new Game();
 const game2 = new GamePart2();
 const day3 = new Day3();
+const day3part2 = new Day3Part2();
